d5123af0d5683a8bd3c720d6d9766a09eae03ba3
Augusto <augustocatterino@gmail.com>
2023-09-14 14:30:29 -0300

    Modificación de NavBar y diseño de publicaciones

--- public/js/home.js
+++ public/js/home.js
@@ -11,8 +11,8 @@ const mostrarPublicaciones = (publicaciones, elementoHtml) => {
     // Método para recorrer los registros
     publicaciones.forEach(publicacion => {
         secciones += `
-            <section class="d-flex gap-3">
-            <img src="${publicacion.url_imagen}" class="rounded">
+            <section class="d-flex gap-5 border p-3 mb-3">
+            <img src="${publicacion.url_imagen}" class="rounded image-post">
             <div class="d-flex flex-column justify-content-center">
                 <h3 class="mt-3 mb-3">${publicacion.titulo}</h3>
                 <p>${publicacion.descripcion}</p>
