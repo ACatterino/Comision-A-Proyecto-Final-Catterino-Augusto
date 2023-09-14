56ad0421b41cc7c4f37e1b46125502928066d042
Augusto <augustocatterino@gmail.com>
2023-09-14 11:14:18 -0300

    Modificación de Tamaño de publicaciones, titulo, detalle y fecha

--- public/js/home.js
+++ public/js/home.js
@@ -11,12 +11,12 @@ const mostrarPublicaciones = (publicaciones, elementoHtml) => {
     // Método para recorrer los registros
     publicaciones.forEach(publicacion => {
         secciones += `
-            <section class="d-flex gap-2">
-            <img src="${publicacion.url_imagen}" class="rounded" height=200 >
-            <div class="d-flex flex-column justify-content-between">
-                <h5>${publicacion.titulo}</h5>
+            <section class="d-flex gap-3">
+            <img src="${publicacion.url_imagen}" class="rounded">
+            <div class="d-flex flex-column justify-content-center">
+                <h3 class="mt-3 mb-3">${publicacion.titulo}</h3>
                 <p>${publicacion.descripcion}</p>
-                <p>${publicacion.fecha}</p>
+                <p class="mt-3"><b>Fecha:</b> ${publicacion.fecha}</p>
             </div>
             </section>
         `
